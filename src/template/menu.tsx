5ad560fcf1d1385da4fbd04fe449076d20ec9a16
Fernando Soares <fernando.git898@gmail.com>
2023-04-07 05:29:47 -0300

    Foi alterado o menu do portifolio por incompatibilidade do componente

--- src/template/menu.tsx
+++ src/template/menu.tsx
@@ -4,75 +4,161 @@ export default function menu(): React.ReactElement {
   return (
     <>
       <Container style="h-1/6 flex flex-col justify-center py-8 px-6 font-inter font-light text-white">
-        <nav>
-          <div className="max-w-screen-xl flex flex-wrap items-center justify-between mx-auto p-4">
-            <a href="#" className="flex items-center">
-            <i className="fa-solid fa-terminal mx-3" style={{color: "#ffffff"}}></i>
-              <span className="self-center text-2xl font-semibold whitespace-nowrap text-white">
-                Dev Front-end
-              </span>
-            </a>
-            <button
-              data-collapse-toggle="navbar-default"
-              type="button"
-              className="inline-flex items-center p-2 ml-3 text-sm rounded-lg md:hidden focus:outline-none focus:ring-2 text-gray-400 hover:bg-gray-700 focus:ring-gray-600"
-              aria-controls="navbar-default"
-              aria-expanded="false"
+        <div className="w-full flex flex-wrap items-center justify-between mx-auto p-4">
+          <a href="#" className="flex items-center">
+            <i
+              className="fa-solid fa-terminal mx-3 text-white"
+            ></i>
+            <span className="self-center text-2xl font-semibold whitespace-nowrap text-white">
+              Dev Front-end
+            </span>
+          </a>
+          <button
+            className="inline-flex items-center p-2 ml-3 text-sm rounded-lg focus:outline-none focus:ring-2 text-gray-400 hover:bg-gray-700 focus:ring-gray-600"
+            type="button"
+            data-drawer-target="drawer-navigation"
+            data-drawer-show="drawer-navigation"
+            aria-controls="drawer-navigation"
+          >
+            <span className="sr-only">Open main menu</span>
+            <svg
+              className="w-6 h-6"
+              aria-hidden="true"
+              fill="currentColor"
+              viewBox="0 0 20 20"
+              xmlns="http://www.w3.org/2000/svg"
             >
-              <span className="sr-only">Open main menu</span>
-              <svg
-                className="w-6 h-6"
-                aria-hidden="true"
-                fill="currentColor"
-                viewBox="0 0 20 20"
-                xmlns="http://www.w3.org/2000/svg"
-              >
-                <path
-                  fillRule="evenodd"
-                  d="M3 5a1 1 0 011-1h12a1 1 0 110 2H4a1 1 0 01-1-1zM3 10a1 1 0 011-1h12a1 1 0 110 2H4a1 1 0 01-1-1zM3 15a1 1 0 011-1h12a1 1 0 110 2H4a1 1 0 01-1-1z"
-                  clipRule="evenodd"
-                ></path>
-              </svg>
-            </button>
-            <div className="hidden w-full md:block md:w-auto" id="navbar-default">
-              <ul className="font-medium flex flex-col p-4 md:p-0 mt-4 border rounded-lg md:flex-row md:space-x-8 md:mt-0 md:border-0 border-gray-700">
-                <li>
-                  <a
-                    href="#sobre"
-                    className="block py-2 pl-3 pr-4 rounded md:bg-transparent md:p-0 text-white"
-                    aria-current="page"
+              <path
+                fill-rule="evenodd"
+                d="M3 5a1 1 0 011-1h12a1 1 0 110 2H4a1 1 0 01-1-1zM3 10a1 1 0 011-1h12a1 1 0 110 2H4a1 1 0 01-1-1zM3 15a1 1 0 011-1h12a1 1 0 110 2H4a1 1 0 01-1-1z"
+                clip-rule="evenodd"
+              ></path>
+            </svg>
+          </button>
+        </div>
+
+        <div
+          id="drawer-navigation"
+          className="fixed top-0 left-0 z-40 h-screen p-4 overflow-y-auto transition-transform -translate-x-full w-full bg-[#1A1A1A]"
+          tabIndex={-1}
+          aria-labelledby="drawer-navigation-label"
+        >
+          <h5
+            id="drawer-navigation-label"
+            className="text-base font-semibold uppercase text-gray-400"
+          >
+            Menu
+          </h5>
+          <button
+            type="button"
+            data-drawer-hide="drawer-navigation"
+            aria-controls="drawer-navigation"
+            className="text-gray-400 bg-transparent rounded-lg text-sm p-1.5 absolute top-2.5 right-2.5 inline-flex items-center hover:bg-gray-600 hover:text-white"
+          >
+            <svg
+              aria-hidden="true"
+              className="w-5 h-5"
+              fill="currentColor"
+              viewBox="0 0 20 20"
+              xmlns="http://www.w3.org/2000/svg"
+            >
+              <path
+                fillRule="evenodd"
+                d="M4.293 4.293a1 1 0 011.414 0L10 8.586l4.293-4.293a1 1 0 111.414 1.414L11.414 10l4.293 4.293a1 1 0 01-1.414 1.414L10 11.414l-4.293 4.293a1 1 0 01-1.414-1.414L8.586 10 4.293 5.707a1 1 0 010-1.414z"
+                clipRule="evenodd"
+              ></path>
+            </svg>
+            <span className="sr-only">Close menu</span>
+          </button>
+          <div className="py-4 overflow-y-auto">
+            <ul className="space-y-2 font-medium">
+            <li>
+                <a
+                  href="#"
+                  className="flex items-center p-2 rounded-lg text-white hover:bg-gray-700"
+                >
+                  <svg
+                    aria-hidden="true"
+                    className="flex-shrink-0 w-6 h-6 transition duration-75 text-gray-400 group-hover:text-white"
+                    fill="currentColor"
+                    viewBox="0 0 20 20"
+                    xmlns="http://www.w3.org/2000/svg"
                   >
-                    Sobre
-                  </a>
-                </li>
-                <li>
-                  <a
-                    href="#experiencia"
-                    className="block py-2 pl-3 pr-4 rounded md:bg-transparent md:p-0 text-white"
+                    <path
+                      fillRule="evenodd"
+                      d="M5 4a3 3 0 00-3 3v6a3 3 0 003 3h10a3 3 0 003-3V7a3 3 0 00-3-3H5zm-1 9v-1h5v2H5a1 1 0 01-1-1zm7 1h4a1 1 0 001-1v-1h-5v2zm0-4h5V8h-5v2zM9 8H4v2h5V8z"
+                      clipRule="evenodd"
+                    ></path>
+                  </svg>
+                  <span className="flex-1 ml-3 whitespace-nowrap">Sobre</span>
+                </a>
+              </li>
+              <li>
+                <a
+                  href="#"
+                  className="flex items-center p-2 rounded-lg text-white hover:bg-gray-700"
+                >
+                  <svg
+                    aria-hidden="true"
+                    className="flex-shrink-0 w-6 h-6 transition duration-75 text-gray-400 group-hover:text-white"
+                    fill="currentColor"
+                    viewBox="0 0 20 20"
+                    xmlns="http://www.w3.org/2000/svg"
                   >
-                    Experiência
-                  </a>
-                </li>
-                <li>
-                  <a
-                    href="#projetos"
-                    className="block py-2 pl-3 pr-4 rounded md:bg-transparent md:p-0 text-white"
+                    <path
+                      fillRule="evenodd"
+                      d="M5 4a3 3 0 00-3 3v6a3 3 0 003 3h10a3 3 0 003-3V7a3 3 0 00-3-3H5zm-1 9v-1h5v2H5a1 1 0 01-1-1zm7 1h4a1 1 0 001-1v-1h-5v2zm0-4h5V8h-5v2zM9 8H4v2h5V8z"
+                      clipRule="evenodd"
+                    ></path>
+                  </svg>
+                  <span className="flex-1 ml-3 whitespace-nowrap">Experiência</span>
+                </a>
+              </li>
+              <li>
+                <a
+                  href="#"
+                  className="flex items-center p-2 rounded-lg text-white hover:bg-gray-700"
+                >
+                  <svg
+                    aria-hidden="true"
+                    className="flex-shrink-0 w-6 h-6 transition duration-75 text-gray-400 group-hover:text-white"
+                    fill="currentColor"
+                    viewBox="0 0 20 20"
+                    xmlns="http://www.w3.org/2000/svg"
                   >
-                    Projetos
-                  </a>
-                </li>
-                <li>
-                  <a
-                    href="#contato"
-                    className="block py-2 pl-3 pr-4 rounded md:bg-transparent md:p-0 text-white"
+                    <path
+                      fillRule="evenodd"
+                      d="M5 4a3 3 0 00-3 3v6a3 3 0 003 3h10a3 3 0 003-3V7a3 3 0 00-3-3H5zm-1 9v-1h5v2H5a1 1 0 01-1-1zm7 1h4a1 1 0 001-1v-1h-5v2zm0-4h5V8h-5v2zM9 8H4v2h5V8z"
+                      clipRule="evenodd"
+                    ></path>
+                  </svg>
+                  <span className="flex-1 ml-3 whitespace-nowrap">Projetos</span>
+                </a>
+              </li>
+              <li>
+                <a
+                  href="#"
+                  className="flex items-center p-2 rounded-lg text-white hover:bg-gray-700"
+                >
+                  <svg
+                    aria-hidden="true"
+                    className="flex-shrink-0 w-6 h-6 transition duration-75 text-gray-400 group-hover:text-white"
+                    fill="currentColor"
+                    viewBox="0 0 20 20"
+                    xmlns="http://www.w3.org/2000/svg"
                   >
-                    Contato
-                  </a>
-                </li>
-              </ul>
-            </div>
+                    <path
+                      fillRule="evenodd"
+                      d="M5 4a3 3 0 00-3 3v6a3 3 0 003 3h10a3 3 0 003-3V7a3 3 0 00-3-3H5zm-1 9v-1h5v2H5a1 1 0 01-1-1zm7 1h4a1 1 0 001-1v-1h-5v2zm0-4h5V8h-5v2zM9 8H4v2h5V8z"
+                      clipRule="evenodd"
+                    ></path>
+                  </svg>
+                  <span className="flex-1 ml-3 whitespace-nowrap">Contato</span>
+                </a>
+              </li>
+            </ul>
           </div>
-        </nav>
+        </div>
       </Container>
     </>
   );
